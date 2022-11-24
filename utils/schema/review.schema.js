c39c808d2d85dbb700cfddf4f7ee25d5c8353f91
MdMuzahid07 <mdmuzahid7396@gmail.com>
2022-11-24 13:33:17 +0600

    review schema update

--- utils/schema/review.schema.js
+++ utils/schema/review.schema.js
@@ -5,9 +5,17 @@ const reviewSchema = mongoose.Schema({
         type: String,
         required: [true, "user name must be added"]
     },
+    userTitle: {
+        type: String,
+        required: [true, "user name must be added"]
+    },
     userAvatar: {
         type: String,
         required: [true, "user avatar must be added"]
+    },
+    userEmail: {
+        type: String,
+        required: [true, "user email must be added"]
     },
     userReview: {
         type: String,
